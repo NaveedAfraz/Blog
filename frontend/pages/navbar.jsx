83c0e3ee0e2d53ac16d60a21ac03c01b1fdf1a00
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-02 22:20:56 +0530

    changed the editshow

--- frontend/pages/navbar.jsx
+++ frontend/pages/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../components/img/logo.png"; // Update this path to where your logo image is stored
 import { authContext } from "../context/authContext";
