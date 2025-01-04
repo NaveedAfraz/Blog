6a280b02d100d20ca9cad7df9aa1f1c6f76da43f
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-04 22:58:24 +0530

    changed

--- frontend/pages/navbar.jsx
+++ frontend/pages/navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = ({ children }) => {
             <button
               onClick={() => {
                 setDropdownActive((prev) => !prev);
-                logout;
+                logout();
               }}
               className="navbar-button"
             >
