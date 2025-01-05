6e64f682d3cb92a7185f8642d1a6246cc08f4042
NaveedAfraz <naveedafraz123@gmail.com>
2025-01-05 13:00:53 +0530

    nothing

--- frontend/components/Editprofile.jsx
+++ frontend/components/Editprofile.jsx
@@ -104,7 +104,7 @@ const EditProfile = ({
         }
       );
       console.log("running2");
-      console.log(res);
+     // console.log(res);
       console.log("this is the data i got from backend ", res.data?.user);
       if (res.status === 200) {
         console.log("Setting user changed", res.data?.user);
@@ -134,7 +134,7 @@ const EditProfile = ({
         }, 4000);
       }
     }
-  }, [showpopup]);
+  }, []);
 
   return (
     <>
